10ee3d15aa9644558770472a14cda21edbf561aa
Ben Wilber <benwilber@gmail.com>
2021-01-26 18:14:25 -0500

    Redirect the homepage to the admin

--- grunge/urls.py
+++ grunge/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 
 from .viewsets import AlbumViewSet, ArtistViewSet, TrackViewSet
@@ -13,7 +14,10 @@ class APIRouter(DefaultRouter):
 
 
 if settings.DJANGO_ADMIN_ENABLED:
-    urlpatterns += [path("admin/", admin.site.urls)]
+    urlpatterns += [
+        re_path("^$", RedirectView.as_view(url="/admin/", permanent=True)),
+        path("admin/", admin.site.urls),
+    ]
 
 if settings.DJANGO_API_ENABLED:
     api_router = APIRouter(trailing_slash=False)
